350a0d22b22105c896e2aae223ed7bf274c1d0d8
Blair Crumbly <blaircrumbly@gmail.com>
2025-01-07 18:22:34 -0500

    11 Tests passed

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+
+
+
+function returnFirstTwoDrivers(drivers){
+    return drivers.slice(0,2)
+}
+
+function returnLastTwoDrivers(drivers){
+    return drivers.slice(-2)
+}
+
+const  selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+//!this is a partially applied function that leverages closures
+//! so when we invoke the outter function the inner one is returned (not invoked)
+//! since the inner one is a closure it will always remember the value of int
+
+const createFareMultiplier = (int) => {
+    return (fare) => int*fare
+
+}
+
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+
+console.log(returnFirstTwoDrivers)
+console.log(returnLastTwoDrivers)
